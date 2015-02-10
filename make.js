2201371dda32305fee7b4bd5064d77b1dc9fe292
secretrobotron <secretrobotron@gmail.com>
2015-02-10 17:16:10 -0500

    wip

--- make.js
+++ make.js
@@ -1,1 +1,19 @@
-var podcast = require('podcast');
+var Podcast = require('podcast');
+var fs = require('fs');
+
+fs.readFile('./webmaker.json', 'utf8', function (err, feedFileData) {
+  fs.readFile('./podcasts.json', 'utf8', function (err, listFileData) {
+
+    var feedOptions = JSON.parse(feedFileData);
+    var podcastList = JSON.parse(listFileData);
+
+    var feed = new Podcast(feedOptions);
+
+    podcastList.forEach(function (entry) {
+      feed.item(entry);
+    });
+
+    var xml = feed.xml();
+    // fs.writeFile('./feed.xml', xml);
+  });
+});
