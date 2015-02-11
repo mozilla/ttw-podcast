07a49e1fc4f88b68c6cd723d9284e31a761e267a
secretrobotron <secretrobotron@gmail.com>
2015-02-11 08:36:38 -0500

    finished v1
    1. moved to rss instead of podcast wrapper
    2. cleaned up filenames
    3. auto-guid for episodes.json
    4. itunes-compliant tags

--- make.js
+++ make.js
@@ -1,19 +1,46 @@
-var Podcast = require('podcast');
+var RSS = require('rss');
 var fs = require('fs');
+var uuid = require('node-uuid');
 
-fs.readFile('./webmaker.json', 'utf8', function (err, feedFileData) {
-  fs.readFile('./podcasts.json', 'utf8', function (err, listFileData) {
+var rewritePodcastList = false;
 
-    var feedOptions = JSON.parse(feedFileData);
-    var podcastList = JSON.parse(listFileData);
+var feedDescriptionFileName = './feed.json';
+var episodeListFileName = './episodes.json';
+var outputFileName = './feed.xml';
 
-    var feed = new Podcast(feedOptions);
+fs.readFile(feedDescriptionFileName, 'utf8', function (err, feedDescriptionFileData) {
+  if (err) {
+    console.error('Can\'t open feed description json file:');
+    console.error(err);
+  }
+  else {
+    fs.readFile(episodeListFileName, 'utf8', function (err, episodeListFileData) {
 
-    podcastList.forEach(function (entry) {
-      feed.item(entry);
+      if (err) {
+        console.error('Can\'t open episode list json file:');
+        console.error(err);
+      }
+      else {
+        var feedDescription = JSON.parse(feedDescriptionFileData);
+        var episodeList = JSON.parse(episodeListFileData);
+
+        var feed = new RSS(feedDescription);
+
+        episodeList.forEach(function (entry) {
+          if (!entry.guid) {
+            entry.guid = uuid.v4();
+            rewritePodcastList = true;
+          }
+          feed.item(entry);
+        });
+
+        var xml = feed.xml({indent: true});
+        fs.writeFile(outputFileName, xml);
+
+        if (rewritePodcastList) {
+          fs.writeFile(episodeListFileName, JSON.stringify(episodeList, null, 2));
+        }
+      }
     });
-
-    var xml = feed.xml();
-    // fs.writeFile('./feed.xml', xml);
-  });
+  }
 });
